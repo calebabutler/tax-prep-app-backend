08d3c2e2ab63c34cd4ac22744d4a5aacbe4de8d8
Caleb Butler <butlercq@gmail.com>
2024-04-10 14:46:01 -0400

    Create a Spring Boot project
    I created a spring boot project with both mongodb (commented out) and
    jpa, since we don't know which database we are using yet.

---
+++ tax-prep-app/src/test/java/com/skillstorm/taxprepapp/TaxPrepAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.taxprepapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxPrepAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
