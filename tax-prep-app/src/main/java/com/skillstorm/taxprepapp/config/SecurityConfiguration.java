7f66535acec3f954c526d4fdfafaf811257a1767
Caleb Butler <butlercq@gmail.com>
2024-04-22 17:30:23 -0400

    Begin a security filter chain

--- tax-prep-app/src/main/java/com/skillstorm/taxprepapp/config/SecurityConfiguration.java
+++ tax-prep-app/src/main/java/com/skillstorm/taxprepapp/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.skillstorm.taxprepapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,12 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((request) ->
+            request.requestMatchers("finances").authenticated()
+                   .requestMatchers("profiles").authenticated()
+                   .requestMatchers("users").authenticated()
+        ).formLogin(Customizer.withDefaults());
+
         return http.build();
     }
 
