da1d687575d5cd7e5446fa5b3d431fd3007f3987
Caleb Butler <butlercq@gmail.com>
2024-04-24 16:20:54 -0400

    Remove BaseController and BaseService

--- tax-prep-app/src/main/java/com/skillstorm/taxprepapp/controllers/FinanceInfoController.java
+++ tax-prep-app/src/main/java/com/skillstorm/taxprepapp/controllers/FinanceInfoController.java
@@ -1,19 +1,10 @@
 package com.skillstorm.taxprepapp.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.skillstorm.taxprepapp.models.FinanceInfo;
-import com.skillstorm.taxprepapp.services.FinanceInfoService;
-
 @RestController
 @RequestMapping("/finances")
-public class FinanceInfoController extends BaseController<FinanceInfo> {
-
-    @Autowired
-    void setService(FinanceInfoService service) {
-        this.service = service;
-    }
+public class FinanceInfoController {
 
 }
--- tax-prep-app/src/main/java/com/skillstorm/taxprepapp/services/FinanceInfoService.java
+++ tax-prep-app/src/main/java/com/skillstorm/taxprepapp/services/FinanceInfoService.java
@@ -1,35 +1,8 @@
 package com.skillstorm.taxprepapp.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.skillstorm.taxprepapp.models.FinanceInfo;
-import com.skillstorm.taxprepapp.repositories.FinanceInfoRepository;
-
 @Service
-public class FinanceInfoService extends BaseService<FinanceInfo> {
-
-    @Override
-    public boolean isValidForCreate(FinanceInfo info) {
-        return info.getId() == null && info.getFilingStatus() != null && info.getSpouseFirstName() != null
-                && info.getSpouseMiddleInitial() != null && info.getSpouseLastName() != null
-                && info.getSpouseSsn() != null && info.getW2Income() != null && info.getOtherIncome() != null
-                && info.getTaxWithheldW2() != null && info.getTaxWithheld1099() != null
-                && info.getTaxWithheldOther() != null && info.getPrevTaxesPaid() != null && info.getProfile() != null;
-    }
-
-    @Override
-    public boolean isValidForUpdate(Integer id, FinanceInfo info) {
-        if (id != null && info.getId() == null) {
-            info.setId(id);
-            return true;
-        }
-        return false;
-    }
-
-    @Autowired
-    void setRepository(FinanceInfoRepository repository) {
-        this.repository = repository;
-    }
+public class FinanceInfoService {
 
 }
