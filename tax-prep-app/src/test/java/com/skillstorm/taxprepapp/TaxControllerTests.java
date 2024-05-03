08b97f41f45761133e1f85502d4894709d998d5b
Caleb Butler <butlercq@gmail.com>
2024-05-03 16:50:57 -0400

    Add tax controller tests

---
+++ tax-prep-app/src/test/java/com/skillstorm/taxprepapp/TaxControllerTests.java
@@ -0,0 +1,134 @@
+package com.skillstorm.taxprepapp;
+
+import java.security.Principal;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import com.skillstorm.taxprepapp.controllers.FinanceInfoController;
+import com.skillstorm.taxprepapp.controllers.ProfileController;
+import com.skillstorm.taxprepapp.controllers.TaxController;
+import com.skillstorm.taxprepapp.models.FinanceInfo;
+import com.skillstorm.taxprepapp.models.Profile;
+import com.skillstorm.taxprepapp.repositories.FinanceInfoRepository;
+import com.skillstorm.taxprepapp.repositories.ProfileRepository;
+
+@SpringBootTest
+public class TaxControllerTests {
+
+    @Autowired
+    ProfileRepository profileRepository;
+
+    @Autowired
+    FinanceInfoRepository financeInfoRepository;
+
+    @Autowired
+    TaxController taxController;
+
+    @Autowired
+    ProfileController profileController;
+
+    @Autowired
+    FinanceInfoController financeInfoController;
+
+    private class ExamplePrincipal implements Principal {
+
+        private String name;
+
+        public ExamplePrincipal(String name) {
+            this.name = name;
+        }
+
+		@Override
+		public String getName() {
+			return name;
+		}
+
+	}
+
+    @Test
+    public void testBadCalculateTaxes() {
+        profileRepository.deleteAll();
+        financeInfoRepository.deleteAll();
+
+        Principal principal = new ExamplePrincipal("testBadCalculateTaxes");
+        ResponseEntity<Long> response = taxController.calculateTaxes(principal);
+        Assertions.assertEquals(response, ResponseEntity.badRequest().build());
+    }
+
+    @Test
+    public void testBadCalculateTaxes2() {
+        profileRepository.deleteAll();
+        financeInfoRepository.deleteAll();
+
+        Principal principal = new ExamplePrincipal("testBadCalculateTaxes2");
+
+        Profile profile = new Profile(
+            null,
+            "Mary",
+            null,
+            "Smith",
+            20010101,
+            "123 Sesame St",
+            "Springfield",
+            "FL",
+            null,
+            11111,
+            111111111,
+            null
+        );
+        profileController.createProfile(principal, profile);
+
+        ResponseEntity<Long> response = taxController.calculateTaxes(principal);
+        Assertions.assertEquals(response, ResponseEntity.badRequest().build());
+    }
+
+    @Test
+    public void testCalculateTaxesSingle() {
+        profileRepository.deleteAll();
+        financeInfoRepository.deleteAll();
+
+        Principal principal = new ExamplePrincipal("testBadCalculateTaxes");
+
+        Profile profile = new Profile(
+            null,
+            "Mary",
+            null,
+            "Smith",
+            20010101,
+            "123 Sesame St",
+            "Springfield",
+            "FL",
+            null,
+            11111,
+            111111111,
+            null
+        );
+
+        FinanceInfo info = new FinanceInfo(
+            "Single",
+            null,
+            null,
+            null,
+            null,
+            null,
+            100000000,
+            2000,
+            2000,
+            2000,
+            2000,
+            2000,
+            null
+        );
+
+        profileController.createProfile(principal, profile);
+        financeInfoController.createInfo(principal, info);
+
+        ResponseEntity<Long> response = taxController.calculateTaxes(principal);
+        Assertions.assertEquals(response, ResponseEntity.ok((long) 32865390));
+    }
+
+}
